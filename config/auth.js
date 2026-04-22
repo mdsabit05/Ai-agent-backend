3b573941b08105ac0791536e94f894bf546ade29
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-22 23:20:46 +0530

    Add files via upload

--- config/auth.js
+++ config/auth.js
@@ -19,6 +19,8 @@ import { betterAuth }  from "better-auth";
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
 
+  baseURL: "https://ai-agent-backend-1-d43j.onrender.com", 
+
   emailAndPassword: {
     enabled: true,
   },
@@ -26,7 +28,7 @@ export const auth = betterAuth({
   trustedOrigins: [
     "http://localhost:3000",
     "https://ai-agent-frontend-up4l.vercel.app",
-    // "https://ai-agent-frontend-up4l-git-main-mdsabit05s-projects.vercel.app"
+    "https://ai-agent-frontend-up4l-git-main-mdsabit05s-projects.vercel.app"
   ],
 
   session: {
