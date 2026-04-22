2597571d38d829faa07293ec446b767ade48f166
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-22 22:27:44 +0530

    Add files via upload

--- config/auth.js
+++ config/auth.js
@@ -2,17 +2,36 @@
 
 import { betterAuth }  from "better-auth";
 
- export const auth = betterAuth({
-    secret : process.env.BETTER_AUTH_SECRET,
+//  export const auth = betterAuth({
+//     secret : process.env.BETTER_AUTH_SECRET,
 
-    emailAndPassword : {
-      enabled : true
-    },
-      trustedOrigins: ["http://localhost:3000",
-  "https://ai-agent-frontend-up4l.vercel.app",
-  "https://ai-agent-frontend-up4l-git-main-mdsabit05s-projects.vercel.app"]
+//     emailAndPassword : {
+//       enabled : true
+//     },
+//       trustedOrigins: ["http://localhost:3000",
+//   "https://ai-agent-frontend-up4l.vercel.app",
+//   "https://ai-agent-frontend-up4l-git-main-mdsabit05s-projects.vercel.app"]
       
-  });
+//   });
 
 
 
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://ai-agent-frontend-up4l.vercel.app"
+  ],
+
+  session: {
+    cookie: {
+      sameSite: "none",
+      secure: true
+    }
+  }
+});
