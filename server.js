f3a3d95a288a32d371d3133d431606816b5fcafc
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-22 15:44:39 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -95,6 +95,8 @@ app.use("/api/history" , async (req, res) => {
 //   res.json({ message: "Logged out" });
 // });
 
-app.listen(5000, () => {
-  console.log("Server running");
-});
+// app.listen(5000, () => {
+//   console.log("Server running");
+// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
