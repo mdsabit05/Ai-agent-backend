1a563fc95056482e9fcd98a5d314ed8569c5845b
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-22 19:58:44 +0530

    Add files via upload

--- config/auth.js
+++ config/auth.js
@@ -8,7 +8,11 @@ import { betterAuth }  from "better-auth";
     emailAndPassword : {
       enabled : true
     },
-      trustedOrigins: ["http://localhost:3000"]
+      trustedOrigins: ["http://localhost:3000",
+  "https://ai-agent-frontend-up4l.vercel.app",
+  "https://ai-agent-frontend-up4l-git-main-mdsabit05s-projects.vercel.app"]
+      
   });
+
 
 
